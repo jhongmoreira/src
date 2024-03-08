1185cac5acfdaa64d7dcfed387fb8506ea3b4aab
Jhonathan Moreira <arte.jhonathan@gmail.com>
2024-03-08 08:39:07 -0300

    Menu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -53,21 +53,21 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Interface
+                Gestão
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <i class="fas fa-fw fa-user"></i>
+                    <span>Clientes</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
+                        <h6 class="collapse-header">Gerir clientes</h6>
+                        <a class="collapse-item" href="buttons.html">Cadastrar</a>
+                        <a class="collapse-item" href="cards.html">Consultar</a>
                     </div>
                 </div>
             </li>
@@ -76,17 +76,15 @@
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
+                    <i class="fas fa-fw fa-file"></i>
+                    <span>Arquivos</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
+                        <h6 class="collapse-header">Gestão de arquivos</h6>
+                        <a class="collapse-item" href="utilities-color.html">Google Drive</a>
+                        <a class="collapse-item" href="utilities-border.html">Pasta Interna</a>
                     </div>
                 </div>
             </li>
@@ -96,26 +94,21 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Addons
+                Vendas
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                     aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
+                    <i class="fas fa-fw fa-money-bill"></i>
+                    <span>Vendas</span>
                 </a>
                 <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Login Screens:</h6>
-                        <a class="collapse-item" href="login.html">Login</a>
-                        <a class="collapse-item" href="register.html">Register</a>
-                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                        <div class="collapse-divider"></div>
-                        <h6 class="collapse-header">Other Pages:</h6>
-                        <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item" href="blank.html">Blank Page</a>
+                        <h6 class="collapse-header">Vendas</h6>
+                        <a class="collapse-item" href="login.html">Nova</a>
+                        <a class="collapse-item" href="register.html">Consultar</a>
                     </div>
                 </div>
             </li>
