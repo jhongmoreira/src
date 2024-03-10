ed14a54909c3ee3df1d329b8c46fb6c917f9af5a
Jhonathan Moreira <arte.jhonathan@gmail.com>
2024-03-10 19:03:25 -0300

    Dashboard com tarefas

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Venda;
+use App\Models\Order;
 
 
 class Dashboard extends Controller
@@ -11,7 +12,9 @@ class Dashboard extends Controller
     public function index(){
         $caixa = Venda::where('pago', 1)->sum('valor_final');
         $vencer = Venda::where('pago', 0)->sum('valor_final');
+        $order = Order::where('finalizado', 0)->count();
+        $orderOk = Order::where('finalizado', 1)->count();
 
-        return view('welcome',['caixa'=>$caixa, 'vencer'=>$vencer]);
+        return view('welcome',['caixa'=>$caixa, 'vencer'=>$vencer, 'order'=>$order, 'orderOk'=>$orderOk]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,6 +49,42 @@
     </div>
 
     <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-danger shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                           Tarefas Pendentes</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $order }} pendentes</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-list fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-info shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                           Tarefas Concluídas</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $orderOk }} concluídas</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-check fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Earnings (Monthly) Card Example -->
     <!-- <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-info shadow h-100 py-2">
             <div class="card-body">
