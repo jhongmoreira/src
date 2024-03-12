9aa2fad39851601c0d6de5cb51180a76a4dcc10f
Jhonathan Moreira <arte.jhonathan@gmail.com>
2024-03-12 09:19:03 -0300

    Botão view cliente removido, link colocado direto no nome

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -14,31 +14,30 @@
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                         <thead>
                             <tr>
+                                <th>#</th>
                                 <th>Nome</th>
                                 <th>CPF</th>
                                 <th>Whatsapp</th>
-                                <th>E-mail</th>
                                 <th>Ação</th>
                             </tr>
                         </thead>
                         <tfoot>
                             <tr>
+                                <th>#</th>
                                 <th>Nome</th>
                                 <th>CPF</th>
                                 <th>Whatsapp</th>
-                                <th>E-mail</th>
                                 <th>Ação</th>
                             </tr>
                         </tfoot>
                         <tbody>
                             @foreach($clientes as $cliente)
                             <tr>
-                                <td>{{ $cliente->nome }}</td>
+                                <td>{{ $cliente->id }}</td>
+                                <td><a href="{{ route('cliente', $cliente->id) }}">{{ $cliente->nome }}</td>
                                 <td>{{ $cliente->cpf }}</td>
-                                <td>{{ $cliente->whatsapp }}</td>
-                                <td>{{ $cliente->email }}</td>
+                                <td><a href="https://wa.me/+55{{$cliente->whatsapp}}" target="_blank">{{$cliente->whatsapp}}</a></td>
                                 <td>
-                                    <a href="{{ route('cliente', $cliente->id) }}" class="btn btn-sm btn-primary mt-1"><i class="fa fa-eye"></i></a>
                                     <a href="{{ $cliente->pasta }}" target="_blank" class="btn btn-sm btn-secondary mt-1"><i class="fa fa-folder"></i></a>
                                     <a href="{{ route('editar-cliente', $cliente->id) }}" class="btn btn-sm btn-warning mt-1"><i class="fa fa-edit"></i></a>
                                     <a href="{{ route('apagar-cliente', $cliente->id) }}" class="btn btn-sm btn-danger mt-1"><i class="fa fa-ban"></i></a>
