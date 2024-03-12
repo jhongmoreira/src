439c7cc25a8c5ec7bfeb9526ac62e08b6efb13cc
Jhonathan Moreira <arte.jhonathan@gmail.com>
2024-03-12 11:01:46 -0300

    Ajuste

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -1,18 +1,34 @@
 @extends('layouts.main')
 @section('title','Detalhe do Cliente - Exata TI')
 @section('content')
-<div class="row">
-    <div class="col-md-12"><h3>Usuário</h3></div>
+
+<div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">
+                    Dados do usuário logado
+                </h6>
+            </div>
+
+            <div class="card-body"> 
+                
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                    <label for="cpf"><b>Nome</b></label>
+                    <p>{{ Auth::user()->name }}</p>
+                    </div>
+                </div>   
+    
+                <div class="col-md-4">
+                    <div class="form-group">
+                    <label for="cpf"><b>E-mail</b></label>
+                    <p>{{ Auth::user()->email }}</p>
+                    </div>
+                </div>                           
+            </div>
+
+
+            </div>
 </div>
 
-<div class="row">
-
-    <div class="col-md-4">
-        <div class="form-group">
-          <label for="cpf"><b>CPF</b></label>
-          <p>{{ Auth::user()->name }}</p>
-        </div>
-    </div
-
-</div>
 @endsection
