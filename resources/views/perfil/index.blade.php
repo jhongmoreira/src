9079edd8c105de55a15fc914a2b6172715385315
Jhonathan Moreira <arte.jhonathan@gmail.com>
2024-03-12 11:03:23 -0300

    Empresa

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -31,4 +31,33 @@
             </div>
 </div>
 
+<div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">
+                    Empresa
+                </h6>
+            </div>
+
+            <div class="card-body"> 
+                
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                    <label for="cpf"><b>Nome</b></label>
+                    <p>{{ Auth::user()->name }}</p>
+                    </div>
+                </div>   
+    
+                <div class="col-md-4">
+                    <div class="form-group">
+                    <label for="cpf"><b>E-mail</b></label>
+                    <p>{{ Auth::user()->email }}</p>
+                    </div>
+                </div>                           
+            </div>
+
+
+            </div>
+</div>
+
 @endsection
