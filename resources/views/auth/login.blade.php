14a2bcc799a03202677590276422fdbd2efd39c5
Jhonathan Moreira <arte.jhonathan@gmail.com>
2024-03-13 15:48:30 -0300

    Logo da Exata na página de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,8 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            <!-- <x-authentication-card-logo /> -->
+            <img src="{{ asset('img/logo_exata_dark.png') }}" width="180" alt="">
         </x-slot>
 
         <x-validation-errors class="mb-4" />
