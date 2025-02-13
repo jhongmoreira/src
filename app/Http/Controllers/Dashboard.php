49601fd684808075015e9d2c2ddcc31af36da2ab
Jhonathan Moreira <arte.jhonathan@gmail.com>
2025-02-13 09:48:57 -0300

    Novo atendimento na tela inicial

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Venda;
 use App\Models\Order;
+use App\Models\Cliente;
 
 
 class Dashboard extends Controller
@@ -14,6 +15,7 @@ class Dashboard extends Controller
         $vencer = Venda::where('pago', 0)->sum('valor_final');
         $order = Order::where('finalizado', 0)->orWhere('finalizado', 1)->count();
         $orderOk = Order::where('finalizado', 2)->count();
+        $clientes = Cliente::all();
         
         $dataAtual = date('Y-m-d'); // Obtém a data atual no formato 'YYYY-MM-DD'
         $orderToday = Order::whereDate('data_cadastro', $dataAtual)->count();
@@ -34,6 +36,6 @@ class Dashboard extends Controller
         $tempo = implode(",", $data);
         $somaTotal = implode(",", $valor);
 
-        return view('welcome',['caixa'=>$caixa, 'vencer'=>$vencer, 'order'=>$order, 'orderOk'=>$orderOk, 'orderToday'=>$orderToday, 'tempo'=>$tempo, 'somaTotal'=>$somaTotal]);
+        return view('welcome',['caixa'=>$caixa, 'vencer'=>$vencer, 'order'=>$order, 'orderOk'=>$orderOk, 'orderToday'=>$orderToday, 'tempo'=>$tempo, 'somaTotal'=>$somaTotal, 'clientes'=>$clientes]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,95 @@
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-    <a href="#"  onclick="Convert_HTML_To_PDF();" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-            class="fas fa-download fa-sm text-white-50"></i> Baixar relatório</a>
+    <div class="justify-content-between">        
+        <a href="#"  class="btn btn-sm btn-primary shadow-sm" data-toggle="modal" data-target="#novoAtendimento">
+            <i class="fas fa-plus fa-sm text-white-50"></i> Novo
+        </a>
+    </div>
 </div>
+
+<div class="modal fade" id="novoAtendimento" tabindex="-1" role="dialog" aria-labelledby="novoKanbanLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="novoKanbanLabel">Novo Quadro</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+        
+                    <div class="modal-body">
+                        <form action="{{ route('salvar-ordem') }}" method="post">
+                            @csrf
+                    
+                            <div class="row">                    
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="cliente_id">Contato</label>
+                                        <select name="cliente_id" id="cliente_id" class="form-control" required>
+                                            <option value=""></option>
+                                            @foreach($clientes as $cliente)
+                                            <option value="{{ $cliente->id }}">
+                                                @if($cliente->matricula =='')
+                                                Externo - {{ $cliente->nome }}
+                                                @else
+                                                {{ $cliente->matricula }} - {{ $cliente->nome }}
+                                                @endif
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="data_cadastro">Data</label>
+                                        <input type="date" name="data_cadastro" id="data_cadastro" class="form-control" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="hora_cadastro">Hora</label>
+                                        <input type="time" name="hora_cadastro" id="hora_cadastro" class="form-control" required>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="finalizado">Status</label>
+                                        <select name="finalizado" id="finalizado" class="form-control" required>
+                                            <option value="0">Não iniciado</option>
+                                            <option value="1">Em Andamento</option>
+                                            <option value="2">Finalizado</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>                    
+                    
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="descricao">Observação</label>
+                                        <textarea name="descricao" id="descricao" rows="5" class="form-control" required></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-success">Salvar</button>
+                    </div>
+                    </form>
+                    </div>
+                    </div>
+                    </div>
+
 
 <!-- Content Row -->
 <div class="row" id="relatorio">
